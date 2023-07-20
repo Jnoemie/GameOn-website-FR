82e16eb8a1ba2fd7cb950aa38984756d5b761a36
Noemie Jankowski <noemiejankowski@MacBook-Air-de-Noemie.local>
2023-07-20 13:46:43 +0200

    changement de l'index

--- starterOnly/script/modal.js
+++ starterOnly/script/modal.js
@@ -11,6 +11,31 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const submitBtn = document.querySelector(".btn-submit");
+const form = document.getElementById("validation-form");
+const closeBtnRed = document.getElementById("closeBtnRed");
+const confirmationMsg = document.getElementById("confirmationMsg");
+const content = document.getElementById("content");
+const heroSection = document.querySelector('.hero-section');
+
+// const pour le formulaire et les erreur 
+
+const firstname = document.getElementById("first");
+const firstnameError = document.getElementById("firstError");
+const lastname = document.getElementById("last");
+const lastnameError = document.getElementById("lastError");
+const email = document.getElementById("email");
+const emailError = document.getElementById("emailError");
+const birthdate = document.getElementById("birthdate");
+const birthdateError = document.getElementById("birthdateError");
+const quantity = document.getElementById("quantity");
+const quantityError = document.getElementById("quantityError");
+const location2 = document.getElementsByName("location");
+const locationError = document.getElementById("locationError");
+const conditions = document.getElementById("checkbox1");
+const conditionsError = document.getElementById("conditionsError");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
